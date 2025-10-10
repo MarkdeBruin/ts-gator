a8f415becf05217648386c5473f8f11072539cca
MarkdeBruin <contact@mrkdbrn.com>
2025-10-10 11:28:58 +0200

    getAllFeedsWithUsers query

--- src/lib/db/queries/feed.ts
+++ src/lib/db/queries/feed.ts
@@ -1,5 +1,6 @@
 import { db } from "..";
-import { feeds } from "../schema.js";
+import { feeds, users } from "../schema.js";
+import { eq } from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, userId: string) {
   const [result] = await db
@@ -11,5 +12,19 @@ export async function createFeed(name: string, url: string, userId: string) {
     })
     .returning();
 
+  return result;
+}
+
+export async function getAllFeedsWithUsers() {
+  const result = await db
+    .select({
+      feedId: feeds.id,
+      feedName: feeds.name,
+      feedUrl: feeds.url,
+      userName: users.name,
+    })
+    .from(feeds)
+    .leftJoin(users, eq(feeds.userId, users.id));
+  
   return result;
 }
